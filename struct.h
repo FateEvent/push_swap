f43e25e32a7aba5d99f28b9fb48e8b8db4158343
FateEvent <fabio.venturini.1989@gmail.com>
2022-03-25 13:42:02 +0100

    creates_deletes

--- struct.h
+++ struct.h
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 13:10:47 by faventur          #+#    #+#             */
-/*   Updated: 2022/03/25 13:40:13 by faventur         ###   ########.fr       */
+/*   Updated: 2022/03/25 13:41:51 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ typedef struct s_stack
 t_stack	*ft_lstnew(void *content);
 void	ft_stackpush(t_stack **stack, int n);
 int		ft_stacksize(t_stack *stack);
-void	ft_stackdelone(t_stack *stack, void (*del)(void *));
 void	ft_stackiter(t_stack *stack, void (*f)(int));
+void	ft_stackdelone(t_stack *stack, void (*del)(void *));
 
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
