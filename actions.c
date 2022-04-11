578febe9c4efd04781e23d43c919b0cb6ea51432
FateEvent <fabio.venturini.1989@gmail.com>
2022-04-11 14:05:01 +0200

    push_puzzle_solved

--- actions.c
+++ actions.c
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 21:41:32 by faventur          #+#    #+#             */
-/*   Updated: 2022/04/11 12:16:41 by faventur         ###   ########.fr       */
+/*   Updated: 2022/04/11 14:04:19 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,50 +25,45 @@ void	ft_swap(t_stack *stack)
 	}
 }
 
+static void	ft_push_part2(t_stack *stack1, t_stack *stack2)
+{
+	if (ft_stacksize(stack1) == 1 && stack2->top == NULL)
+	{
+		stack2->top = stack1->top;
+		stack1->top = NULL;
+	}
+	else
+	{
+		stack2->top->prev = stack1->top;
+		stack2->top->prev->next = stack2->top;
+		stack2->top = stack1->top;
+		stack1->top = NULL;
+	}
+}
+
 void	ft_push(t_stack *stack1, t_stack *stack2)
 {
 	if (stack1 != NULL && stack2 != NULL && ft_stacksize(stack1) > 0)
 	{
-		if (stack2->top == NULL)
+		if (ft_stacksize(stack1) >= 2 && stack2->top == NULL)
 		{
 			stack2->top = stack1->top;
 			stack1->top = stack1->top->next;
 			stack1->top->prev = NULL;
 			stack2->top->next = NULL;
 			stack2->top->prev = NULL;
-			ft_printf("a1 %d, a2 %d, b1 %d\n", stack1->top->content, stack1->top->next->content, stack2->top->content);
 		}
-		/*
-		else if (stack2->top == NULL)
+		else if (ft_stacksize(stack1) >= 2 && ft_stacksize(stack2) > 1)
 		{
-			stack2->top = stack1->top;
 			stack1->top = stack1->top->next;
-			stack1->top->prev = NULL;
-			stack2->top->next = NULL;
-			stack2->top->prev = NULL;
-			ft_printf("a1 %d, a2 %d, b1 %d\n", stack1->top->content, stack1->top->next->content, stack2->top->content);
-		}
-		*/
-		else if (ft_stacksize(stack1) >= 2 && ft_stacksize(stack2) >= 1)
-		{
-			stack2->top->prev = stack1->top;
-			stack1->top = stack1->top->next;
+			stack2->top->prev = stack1->top->prev;
+			stack2->top->prev->next = stack2->top;
 			stack2->top = stack2->top->prev;
 			stack2->top->prev = NULL;
 			stack1->top->prev = NULL;
-
-//			stack2->top->next = stack2->top->next->next;
-			ft_printf("a1 %d, a2 %d, b1 %d, b2 %d\n", stack1->top->content, stack1->top->next->content, stack2->top->content, stack2->top->next->content);
-			/*
-			stack1->top->next->next = stack->top->next;
-			stack->top->next = stack->top->prev;
-
-			stack1->top->prev = NULL;
-			
-			stack->top->next->prev = stack->top;
-			stack->top->next->next->prev = stack->top->next;
-			*/
 		}
+		else
+			ft_push_part2(stack1, stack2);
 	}
 }
 
@@ -81,7 +76,6 @@ void	ft_rotate(t_stack *stack)
 		stack->bottom = stack->top;
 		stack->top = stack->top->next;
 		stack->bottom->next = NULL;
-		ft_printf("%d, %d, %d\n", stack->top->content, stack->top->next->content, stack->top->next->next->content);
 	}
 }
 
@@ -94,6 +88,5 @@ void	ft_rev_rotate(t_stack *stack)
 		stack->top = stack->bottom;
 		stack->bottom = stack->bottom->prev;
 		stack->bottom->next = NULL;
-		ft_printf("%d, %d, %d\n", stack->top->content, stack->top->next->content, stack->top->next->next->content);
 	}
 }
