a14a4c2e9a973697d96019ec81f87b3c87b1d478
FateEvent <fabio.venturini.1989@gmail.com>
2022-05-03 17:48:40 +0200

    mustn't

--- libft/srcsb/ft_lstdelone.c
+++ libft/srcsb/ft_lstdelone.c
@@ -6,14 +6,14 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 15:06:32 by faventur          #+#    #+#             */
-/*   Updated: 2022/04/08 22:01:02 by faventur         ###   ########.fr       */
+/*   Updated: 2022/05/03 17:48:10 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 ** The ft_lstdelone() function frees the memory of the element passed
 ** as a parameter with the del function and free(3). The memory of
-** next must be freed.
+** next mustn't be freed.
 */
 
 #include "libft.h"
